26d737f966deaaf3163c2b1f2dffc0c477374521
tony2928 <tonytr91@gmail.com>
2023-09-13 23:27:18 -0600

    m

--- tarea.php
+++ tarea.php
@@ -85,12 +85,42 @@ $materiaClean = str_replace($acentos, $sinAcentos, $materiaClean);
                     <div class="col">
                         <h3>Descripción:</h3>
                         <p><?php echo $descripcion ?></p>
+                        <p><b>Reading text: An email from a friend</b>
+                            <br>
+                            <br>
+                            Hi Samia,
+                            <br>
+                            <br>
+                            Quick email to say that sounds like a great idea. Saturday is better for me because I'm
+                            meeting my parents on Sunday. So if that's still good for you, why don't you come here? Then
+                            you can see the new flat and all the work we've done on the kitchen since we moved in. We
+                            can eat at home and then go for a walk in the afternoon. It's going to be so good to catch
+                            up finally. I want to hear all about your new job!
+                            <br>
+                            <br>
+                            Our address is 52 Charles Road, but it's a bit difficult to find because the house numbers
+                            are really strange here. If you turn left at the post office and keep going past the big
+                            white house on Charles Road, there's a small side street behind it with the houses 50-56 in.
+                            Don't ask me why the side street doesn't have a different name! But call me if you get lost
+                            and I'll come and get you.
+                            <br>
+                            <br>
+                            Let me know if there's anything you do/don't like to eat. Really looking forward to seeing
+                            you!
+                            <br>
+                            <br>
+                            See you soon!
+                            <br>
+                            <br>
+                            Gregor
+                        </p>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col">
-                        <img onclick="viewFull('img/<?php echo $materiaClean ?>/<?php echo $imagen ?>')" class="imagen-tarea" src="img/<?php echo $materiaClean ?>/<?php echo $imagen ?>" alt="">
+                        <img onclick="viewFull('img/<?php echo $materiaClean ?>/<?php echo $imagen ?>')"
+                            class="imagen-tarea" src="img/<?php echo $materiaClean ?>/<?php echo $imagen ?>" alt="">
                     </div>
                 </div>
 
@@ -125,7 +155,7 @@ $materiaClean = str_replace($acentos, $sinAcentos, $materiaClean);
 
 
     </div>
-    
+
     <div id="image-fullscreen" class="image-fullscreen">
 
         <div onclick="exitFull()" id="image-fullscreen-content" class="image-fullscreen-content">
